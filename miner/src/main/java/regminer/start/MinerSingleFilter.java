025c7ec47ca996b12a2a5f44e80351af87a4a7e3
SongXueZhi <sxzdhu@gmail.com>
2021-12-30 23:35:09 +0800

    fix conflict on filter task

--- miner/src/main/java/regminer/start/MinerSingleFilter.java
+++ miner/src/main/java/regminer/start/MinerSingleFilter.java
@@ -5,19 +5,17 @@ import regminer.constant.Conf;
 import regminer.git.provider.Provider;
 import regminer.miner.PotentialBFCDetector;
 import regminer.model.PotentialRFC;
-import regminer.utils.FileUtilx;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class MinerFilter {
+public class MinerSingleFilter {
     public static void main(String[] args) throws Exception {
         long s1 = System.currentTimeMillis();
-        MinerFilter minerFilter = new MinerFilter();
+        MinerSingleFilter minerFilter = new MinerSingleFilter();
         minerFilter.prepare();
-        minerFilter.handleTask();
+        minerFilter.handleSingleTask();
         long s2 = System.currentTimeMillis();
         System.out.println(s2 - s1);
     }
@@ -28,8 +26,9 @@ public class MinerFilter {
         Miner.git = new Git(Miner.repo);
     }
 
-    public void handleTask() throws Exception {
-        List<String> filter = new ArrayList<>(FileUtilx.readSetFromFile(Conf.PROJECT_PATH + File.separator + "bfc.txt"));
+    public void handleSingleTask() throws Exception {
+        List<String> filter = new ArrayList<>();
+        filter.add("e5210d1f9ef4f1d41ff0a8c4a2ab8e9192d5e087");
         PotentialBFCDetector pBFCDetector = new PotentialBFCDetector(Miner.repo, Miner.git);
         Miner.pRFCs = null;
         Miner.pRFCs = (LinkedList<PotentialRFC>) pBFCDetector.detectPotentialBFC(filter);
