ecc9220bcb6f779ba6b99b7f2f011ae941ee3b08
SongXueZhi <sxzdhu@gmail.com>
2022-05-26 14:24:14 +0800

    Removal of obsolete project modules, which will be relaunched after refactoring

--- bfc_detector/src/main/java/Priority.java
+++
@@ -1,8 +0,0 @@
-
-public class Priority {
-
-	public final static int high = 1;
-	public final static int middle = 0;
-	public final static int lower = -1;
-
-}
--- pre_compile/src/main/java/pre_compile/RepositoryProvider.java
+++
@@ -1,7 +0,0 @@
-package pre_compile;
-
-import org.eclipse.jgit.lib.Repository;
-
-public interface RepositoryProvider {
-	Repository get(String clientPath) throws Exception;
-}
