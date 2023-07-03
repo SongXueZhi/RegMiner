7f81e3bd75852149d425dfe7e035b5b02a92bd80
sunyujie <20212010146@fudan.edu.cn>
2023-07-03 23:52:34 +0800

    revert: platform: revert CodeUtil.java to origin

--- platform/src/main/java/com/fudan/annotation/platform/backend/util/CodeUtil.java
+++ platform/src/main/java/com/fudan/annotation/platform/backend/util/CodeUtil.java
@@ -20,9 +20,9 @@ public class CodeUtil {
         ASTParser parser = ASTParser.newParser(AST.JLS13); // handles JDK 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
         parser.setSource(fileContent.toCharArray());
         // In order to parse 1.6 code, some compiler options need to be set to 1.6
-//        Map<String, String> options = JavaCore.getOptions();
-//        JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
-//        parser.setCompilerOptions(options);
+        Map<String, String> options = JavaCore.getOptions();
+        JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
+        parser.setCompilerOptions(options);
 
         CompilationUnit result = (CompilationUnit) parser.createAST(null);
         return result;
