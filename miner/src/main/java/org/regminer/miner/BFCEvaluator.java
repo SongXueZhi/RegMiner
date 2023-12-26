7c123254041fab04b3b64e96dd64d0f519bbcaae
Xuezhi Song <sxzdhu@gmail.com>
2023-12-26 22:15:42 +0800

    refactor BFCEvaluator

--- miner/src/main/java/org/regminer/miner/BFCEvaluator.java
+++ miner/src/main/java/org/regminer/miner/BFCEvaluator.java
@@ -1,5 +1,6 @@
 package org.regminer.miner;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.regminer.common.constant.Configurations;
 import org.regminer.common.constant.Constant;
 import org.regminer.common.model.PotentialBFC;
@@ -62,18 +63,43 @@ public class BFCEvaluator extends BFCSearchStrategy {
         }
     }
 
-
     public void evolute(PotentialBFC pRFC) {
         String bfcID = pRFC.getCommit().getName();
         try {
-            // 1.checkout bfc
+            if (!prepareBFC(pRFC, bfcID)) return;
+
+            Triple<Boolean, CompileResult, CtContext> compileResultTriple = compileBFC(pRFC, bfcID);
+            if (!compileResultTriple.getLeft()) return;
+
+            if (!testBFC(pRFC, bfcID, compileResultTriple.getMiddle(), compileResultTriple.getRight())) return;
+
+            if (!chooseBFCP(pRFC, bfcID)) return;
+
+            updateProgress(bfcID);
+        } catch (Exception e) {
+            handleException(pRFC, bfcID, e);
+        } finally {
+            cleanUp(pRFC, bfcID);
+        }
+    }
+
+    private boolean prepareBFC(PotentialBFC pRFC, String bfcID) {
+        try {
             logger.info("{} checkout ", bfcID);
-            logger.info("commit message:{}",pRFC.getCommit().getShortMessage());
+            logger.info("commit message:{}", pRFC.getCommit().getShortMessage());
             logger.info("before analysis, test case size: {}", pRFC.getTestCaseFiles().size());
             File bfcDirectory = testCaseMigrator.checkoutCiForBFC(bfcID, bfcID);
             pRFC.fileMap.put(bfcID, bfcDirectory);
-            //2. 尝试编译BFC
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    private Triple<Boolean, CompileResult, CtContext> compileBFC(PotentialBFC pRFC, String bfcID) {
+        try {
             CtContext ctContext = new CtContext(new AutoCompileAndTest());
+            File bfcDirectory = pRFC.fileMap.get(bfcID);
             ctContext.setProjectDir(bfcDirectory);
 
             CompileResult compileResult = ctContext.compile(OriginCompileFixWay.values());
@@ -84,95 +110,101 @@ public class BFCEvaluator extends BFCSearchStrategy {
                     pRFC.getTestCaseFiles().clear();
                 }
                 logger.info("BFC compile error");
-                emptyCache(pRFC.fileMap.get(bfcID));
-                return;
+                return Triple.of(false, compileResult, ctContext);
             }
-            //3. 检测 BFC 是否包含测试
-            if (pRFC.getTestCaseFiles().stream()
-                    .noneMatch(testFile -> pRFC.getCommit().getName().equals(testFile.getNewCommitId()))) {
-                logger.info("BFC doesn't contains TestCases, try to migrate");
-                // 不包含当前 commit 中的测试文件时，测试前也需要迁移测试文件
-                // 先迁移，再解析 4. 否则解析结果可能和 git 提供的文件修改记录对应不上
-                Migrator.mergeTwoVersion_BaseLine(pRFC, pRFC.fileMap.get(pRFC.getCommit().getName()));
-            }
-
-            //4. parser Testcase
-            testCaseParser.parseTestCases(pRFC);
-            //5. 测试BFC
-            TestResult testResult = testCaseMigrator.test(pRFC.getTestCaseFiles(), ctContext,
-                    compileResult.getCompileWay());
-
-            TestUtils.retainTestFilesMatchingStates(pRFC, testResult, Arrays.asList(TestCaseResult.TestState.PASS));
-
-            if (pRFC.getTestCaseFiles().isEmpty()) {
-                logger.error("BFC all test fal");
-                emptyCache(pRFC.fileMap.get(bfcID));
-                return;
-            }
-
-            //6. 选择BFCP
-            int count = pRFC.getCommit().getParentCount();
-            if (count == 0) {
-                logger.error("BFC has no parent");
-                emptyCache(pRFC.fileMap.get(bfcID));
-                return;
-            }
-
-            boolean findBFCPFlag = false;
-            for (int i = 0; i < count; i++) {
-                String bfcpID = pRFC.getCommit().getParent(i).getName();
-                logger.info("bfc~1 {} of {}", bfcpID, bfcID);
-                TestResult bfcpTestResult = testCaseMigrator.migrate(pRFC, bfcpID);
-                if (bfcpTestResult == null) { //这说明编译失败
-                    continue;
-                }
-
-                Set<String> matchTestCase = TestUtils.collectTestCases(bfcpTestResult,
-                        testState -> Arrays.asList(TestCaseResult.TestState.FAL,
-                                TestCaseResult.TestState.TE).contains(testState)).keySet();
-
-                if (matchTestCase.isEmpty()) {
-                    continue;
-                }
-                //查找成功，删除无关的测试用例
-                TestUtils.retainTestFilesMatchingFilter(pRFC, matchTestCase);
-                logger.info(bfcpTestResult.getCaseResultMap());
-                logger.info("bfc~1 test fal");
-                testCaseMigrator.purgeUnlessTestcase(pRFC.getTestCaseFiles(), pRFC);//XXX:TestDenpendency:TEST
-                // REDUCE
-                findBFCPFlag = true;
-                pRFC.setBuggyCommitId(bfcpID);
-                break;  //跳出，找到一个就够了
-            }
-            if (!findBFCPFlag) {
-                if (pRFC.getTestCaseFiles() != null) {
-                    pRFC.getTestCaseFiles().clear();
-                }
-                logger.info("Can't find a bfc-1");
-                emptyCache(pRFC.fileMap.get(bfcID));
-            }
-
-            if (Configurations.taskName.equals(Constant.BFC_TASK)) {
-                ProgressMonitor.updateState(bfcID);
-            }
-
-        } catch (Exception e) {
-            if (pRFC.getTestCaseFiles() == null) {
-                logger.error("pbfc test case is null");
-                pRFC.setTestCaseFiles(new ArrayList<>());
-            }
-            logger.info("pbfc test case size: {}", pRFC.getTestCaseFiles().size());
-            pRFC.getTestCaseFiles().clear();
-            emptyCache(pRFC.fileMap.get(bfcID));
-            logger.error(e.getMessage());
-        } 
-        finally {
-            if (Configurations.taskName.equals(Constant.BFC_TASK)) {
-                emptyCache(pRFC.fileMap.get(bfcID));
-            }
+            return Triple.of(true, compileResult, ctContext);
+        } catch (Exception ex) {
+            return Triple.of(false, null, null);
         }
     }
 
+    private boolean testBFC(PotentialBFC pRFC, String bfcID, CompileResult compileResult, CtContext ctContext) {
+        if (pRFC.getTestCaseFiles().stream()
+                .noneMatch(testFile -> pRFC.getCommit().getName().equals(testFile.getNewCommitId()))) {
+            logger.info("BFC doesn't contains TestCases, try to migrate");
+            // 不包含当前 commit 中的测试文件时，测试前也需要迁移测试文件
+            // 先迁移，再解析 4. 否则解析结果可能和 git 提供的文件修改记录对应不上
+            Migrator.mergeTwoVersion_BaseLine(pRFC, pRFC.fileMap.get(pRFC.getCommit().getName()));
+        }
+
+        //4. parser Testcase
+        testCaseParser.parseTestCases(pRFC);
+        //5. 测试BFC
+        TestResult testResult = testCaseMigrator.test(pRFC.getTestCaseFiles(), ctContext,
+                compileResult.getCompileWay());
+        TestUtils.retainTestFilesMatchingStates(pRFC, testResult, List.of(TestCaseResult.TestState.PASS));
+        if (pRFC.getTestCaseFiles().isEmpty()) {
+            logger.error("BFC all test fal");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean chooseBFCP(PotentialBFC pRFC, String bfcID) throws Exception {
+        //6. 选择BFCP
+        int count = pRFC.getCommit().getParentCount();
+        if (count == 0) {
+            logger.error("BFC has no parent");
+            return false;
+        }
+
+        boolean findBFCPFlag = false;
+        for (int i = 0; i < count; i++) {
+            String bfcpID = pRFC.getCommit().getParent(i).getName();
+            logger.info("bfc~1 {} of {}", bfcpID, bfcID);
+            TestResult bfcpTestResult = testCaseMigrator.migrate(pRFC, bfcpID);
+            if (bfcpTestResult == null) { //这说明编译失败
+                continue;
+            }
+
+            Set<String> matchTestCase = TestUtils.collectTestCases(bfcpTestResult,
+                    testState -> Arrays.asList(TestCaseResult.TestState.FAL,
+                            TestCaseResult.TestState.TE).contains(testState)).keySet();
+
+            if (matchTestCase.isEmpty()) {
+                continue;
+            }
+            //查找成功，删除无关的测试用例
+            TestUtils.retainTestFilesMatchingFilter(pRFC, matchTestCase);
+            logger.info(bfcpTestResult.getCaseResultMap());
+            logger.info("bfc~1 test fal");
+            testCaseMigrator.purgeUnlessTestcase(pRFC.getTestCaseFiles(), pRFC);
+            // REDUCE
+            findBFCPFlag = true;
+            pRFC.setBuggyCommitId(bfcpID);
+            break;  //跳出，找到一个就够了
+        }
+        if (!findBFCPFlag) {
+            if (pRFC.getTestCaseFiles() != null) {
+                pRFC.getTestCaseFiles().clear();
+            }
+            logger.info("Can't find a bfc-1");
+            return false;
+        }
+        return true;
+    }
+
+    private void updateProgress(String bfcID) {
+        if (Configurations.taskName.equals(Constant.BFC_TASK)) {
+            ProgressMonitor.updateState(bfcID);
+        }
+    }
+
+    private void handleException(PotentialBFC pRFC, String bfcID, Exception e) {
+        if (pRFC.getTestCaseFiles() == null) {
+            logger.error("pbfc test case is null");
+            pRFC.setTestCaseFiles(new ArrayList<>());
+        }
+        logger.info("pbfc test case size: {}", pRFC.getTestCaseFiles().size());
+        logger.error(e.getMessage());
+        emptyCache(pRFC.fileMap.get(bfcID).getParentFile());
+    }
+
+    private void cleanUp(PotentialBFC pRFC, String bfcID) {
+        if (Configurations.taskName.equals(Constant.BFC_TASK)) {
+            emptyCache(pRFC.fileMap.get(bfcID).getParentFile());
+        }
+    }
 
     public void emptyCache(File file) {
         new SycFileCleanup().cleanDirectory(file.getParentFile());
