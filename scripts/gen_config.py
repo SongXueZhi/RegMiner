5253c2ff7b2a3950c08cd43527d661bd8dafd80f
Xuezhi Song <sxzdhu@gmail.com>
2024-01-08 15:21:35 +0800

    enhance gen_config.py

--- scripts/gen_config.py
+++ scripts/gen_config.py
@@ -4,12 +4,14 @@ from get_jdks import scan_jdks
 # Define the content of the configuration file
 config_content = """
 ###SQL ######
-sql_enable=0
-sql_url=jdbc:mysql://10.177.21.179:3306/code_annotation2?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF8
+sql_enable=1
+sql_url=jdbc:mysql://10.176.34.95:3306/code_annotation2?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF8
 username=root
-passwd=123456
+passwd=1235
 ###jenv ######
 jenv_enable=0
+###enable monitor####
+open_monitor=1
 ###java path ######
 
 """
